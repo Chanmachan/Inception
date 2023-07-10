de6f2199164c7b744ed6df65c1d7d2030370a538
chanma <yunosuke.homma@icloud.com>
2023-07-11 03:21:23 +0900

    軽くね

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
 set -e
+
+# check if database exist
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+  echo "hoge"
+  ## Set mariadb data directory
+  mysql_install_db --user=mysql --datadir=/var/lib/mysql
+  # --userはmysqldを実行するユーザーを指定するオプション
+  # --datadirはMariaDBのデータディレクトリへのパス
+
+fi
