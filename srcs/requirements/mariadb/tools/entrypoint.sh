454106462968079ce8cddb88b2ddc96a682918bc
chanma <yunosuke.homma@icloud.com>
2023-11-12 18:01:15 +0900

    phpは動いていることまで確認した

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -2,11 +2,12 @@
 set -e
 
 # check if database exist
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-  echo "hoge"
+if [ -d "/var/lib/mysql/mysql" ]; then
+  echo "Database already exist"
+else
+  echo "Setting up mariadb database for the first time"
   ## Set mariadb data directory
   mysql_install_db --user=mysql --datadir=/var/lib/mysql
   # --userはmysqldを実行するユーザーを指定するオプション
   # --datadirはMariaDBのデータディレクトリへのパス
-
 fi
