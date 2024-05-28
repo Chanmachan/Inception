a2fe163abed0231af6f4a834cf646ac4436e1ced
chanma <yunosuke.homma@icloud.com>
2024-05-28 11:39:12 +0900

    wordpress、php-fpmのチェックを追加

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,16 +1,19 @@
-# wordpressをインストールして起動する
-echo "Downloading WordPress..."
-wget https://wordpress.org/latest.tar.gz
-echo "Extracting WordPress..."
-tar -xvf latest.tar.gz -C /var/www/html
-echo "WordPress setup complete."
+if [ -d /var/www/html/wordpress ]; then
+    echo "wordpress is already installed"
+else
+    echo "installing wordpress..."
+    wget https://wordpress.org/latest.tar.gz
+    tar -xvf latest.tar.gz -C /var/www/html
+    rm -rf latest.tar.gz
+fi
 
 # PHP-FPMをフォアグラウンドで実行するため
 if pgrep php-fpm7.3 > /dev/null; then
     echo "php-fpm7.3 is already running"
 else
     echo "Starting php-fpm7.3..."
+    # -FはPHP-FPMをフォアグラウンドで実行するため
     exec php-fpm7.3 -F
+    # PID1で実行するため、UNIXシグナル(終了シグナル)を受け取りコンテナを適切に扱える
+    # php-fpm7.3はFastCGI Process Manager (FPM) を実行するコマンド
 fi
-# execコマンドはPID1で実行するため、UNIXシグナル(終了シグナル)を受け取りコンテナを適切に扱える
-# php-fpm7.3はFastCGI Process Manager (FPM) を実行するコマンド
