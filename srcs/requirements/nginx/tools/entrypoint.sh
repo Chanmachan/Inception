495b853102deb919f996bfb67fabb828190c9afe
chanma <yunosuke.homma@icloud.com>
2024-05-30 03:42:38 +0900

    configのプラグインが効かないの不便だったのでファイル名を変更

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 
 # 環境変数をテンプレートに反映して、設定ファイルを生成
-envsubst '$$DOMAIN_NAME' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$$DOMAIN_NAME' < /etc/nginx/template-nginx.conf > /etc/nginx/nginx.conf
 
 exec "$@"
