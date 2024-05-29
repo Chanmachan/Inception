226cf2e37501fc359d8a612e05c3c75f3f502d8d
chanma <yunosuke.homma@icloud.com>
2024-05-29 15:47:22 +0900

    修正

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -2,20 +2,6 @@
 
 set -e
 
-mysqld &
-wait_server_start() {
-  timeout=30
-  while ! mysqladmin ping -u root --silent; do
-  sleep 1
-  timeout=$((timeout-1))
-  if ["$timeout" -le 0]; then
-    echo "server could not start"
-    exit 1
-  fi
-  echo "server started successfully"
-  done
-}
-
 chown -R mysql:mysql /var/lib/mysql
 chown -R mysql:mysql /var/run/mysqld
 if [ ! -d /var/lib/mysql/mysql ]; then
@@ -26,11 +12,22 @@ else
     echo "mariadb is already installed"
 fi
 
-# サーバーをバックグラウンドで実行
+timeout=30
 mysqld &
-# サーバーが起動するまで待つ
-echo "waiting for server to start..."
+wait_server_start() {
+  while ! mysqladmin ping -u root --silent; do
+  sleep 1
+  timeout=$((timeout-1))
+  if [ "$timeout" -le 0 ]; then
+    echo "timeout"
+    exit 1
+  fi
+  echo "server started successfully"
+  done
+}
+
 wait_server_start
+
 mysqladmin shutdown
 
 echo "done settings"
