0bfe88710903f87079d3fda9c464c04a681aa373
chanma <yunosuke.homma@icloud.com>
2024-05-29 18:27:52 +0900

    mysqlの設定

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-set -e
-
-chown -R mysql:mysql /var/lib/mysql
-chown -R mysql:mysql /var/run/mysqld
-if [ ! -d /var/lib/mysql/mysql ]; then
-    echo "setting mariadb for the first time..."
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
-    echo "done setting mariadb"
-else
-    echo "mariadb is already installed"
-fi
-
+######################function######################
 timeout=30
-mysqld &
 wait_server_start() {
   while ! mysqladmin ping -u root --silent; do
   sleep 1
@@ -25,11 +13,40 @@ wait_server_start() {
   echo "server started successfully"
   done
 }
+####################################################
 
+set -e
+
+# 所有権を変更user:group
+# 他のユーザーがデータベースを操作できないようにするため
+chown -R mysql:mysql /var/lib/mysql
+chown -R mysql:mysql /var/run/mysqld
+# mysqlのデータベースがない場合、初期設定
+if [ ! -d /var/lib/mysql/mysql ]; then
+    echo "setting mariadb for the first time..."
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    echo "done setting mariadb"
+else
+    echo "mariadb is already installed"
+fi
+
+# サーバーを起動
+mysqld &
 wait_server_start
-
+# サーバーが起動している->設定   していない->exit
+# rootユーザーのパスワードを設定
+# データベース、ユーザー、権限を設定をまとめてしまった
+if ! mysql -u root -p"${MYSQL_ROOT_PASSWORD}" --silent; then
+  mysql -u root << EOF
+  CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+  CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+  CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+  GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+  FLUSH PRIVILEGES;
+EOF
+fi
+# サーバーを停止
 mysqladmin shutdown
-
 echo "done settings"
 
 exec "$@"
