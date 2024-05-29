45866218620e03222bcbaf49a399fbc4b876a803
chanma <yunosuke.homma@icloud.com>
2024-05-29 15:32:08 +0900

    mariadbの初期設定できるところまで

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,13 +1,38 @@
-#!/bin/sh
+#!/bin/bash
+
 set -e
 
-# check if database exist
-if [ -d "/var/lib/mysql/mysql" ]; then
-  echo "Database already exist"
+mysqld &
+wait_server_start() {
+  timeout=30
+  while ! mysqladmin ping -u root --silent; do
+  sleep 1
+  timeout=$((timeout-1))
+  if ["$timeout" -le 0]; then
+    echo "server could not start"
+    exit 1
+  fi
+  echo "server started successfully"
+  done
+}
+
+chown -R mysql:mysql /var/lib/mysql
+chown -R mysql:mysql /var/run/mysqld
+if [ ! -d /var/lib/mysql/mysql ]; then
+    echo "setting mariadb for the first time..."
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    echo "done setting mariadb"
 else
-  echo "Setting up mariadb database for the first time"
-  ## Set mariadb data directory
-  mysql_install_db --user=mysql --datadir=/var/lib/mysql
-  # --userはmysqldを実行するユーザーを指定するオプション
-  # --datadirはMariaDBのデータディレクトリへのパス
+    echo "mariadb is already installed"
 fi
+
+# サーバーをバックグラウンドで実行
+mysqld &
+# サーバーが起動するまで待つ
+echo "waiting for server to start..."
+wait_server_start
+mysqladmin shutdown
+
+echo "done settings"
+
+exec "$@"
