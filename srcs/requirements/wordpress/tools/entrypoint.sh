48c748559b6e031eb56b33f3db7f58c10eb77113
chanma <yunosuke.homma@icloud.com>
2024-05-30 14:46:58 +0900

    wp-config.phpを-dでチェックしてたのを-eに修正

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # mariadbコンテナが起動したか確認
 timeout=30
-while ! mysql -h $MYSQL_HOST -u root -p$MYSQL_ROOT_PASSWORD 2>/dev/null; do
+while ! mysql -h$MYSQL_HOST -u root -p$MYSQL_ROOT_PASSWORD 2>/dev/null; do
   echo "waiting for mariadb to start..."
   sleep 1
   timeout=$((timeout-1))
@@ -13,7 +13,7 @@ while ! mysql -h $MYSQL_HOST -u root -p$MYSQL_ROOT_PASSWORD 2>/dev/null; do
 done
 
 
-if [ -d /var/www/html/wordpress/wp-config.php ]; then
+if [ -e /var/www/html/wordpress/wp-config.php ]; then
     echo "wordpress is already installed"
 else
     echo "setting wordpress..."
@@ -27,6 +27,12 @@ else
     wp config create --allow-root --path=/var/www/html/wordpress --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST
     echo "installing wordpress..."
     wp core install --allow-root --path=/var/www/html/wordpress --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL
+
+
+
+    chown -R www-data:www-data /var/www/html/wordpress
+    # TODO: 本番環境では削除
+#    chmod 755 /var/www/html/wordpress/test.php
 fi
 
 exec "$@"
