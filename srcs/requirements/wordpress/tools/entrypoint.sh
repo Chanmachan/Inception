9ce71a03814f0eb77683cb3b7a524761cbdc4ed6
chanma <yunosuke.homma@icloud.com>
2024-05-31 02:12:03 +0900

    追加のユーザー作成、見やすくした

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # mariadbコンテナが起動したか確認
 timeout=30
-while ! mysql -h$MYSQL_HOST -u root -p$MYSQL_ROOT_PASSWORD 2>/dev/null; do
+while ! mysql -h$MYSQL_HOST -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_USER_PASSWORD 2>/dev/null; do
   echo "waiting for mariadb to start..."
   sleep 1
   timeout=$((timeout-1))
@@ -24,10 +24,26 @@ else
     chmod +x /usr/local/bin/wp
     wp core download --allow-root --path=/var/www/html/wordpress
     echo "creating wp-config.php ..."
-    wp config create --allow-root --path=/var/www/html/wordpress --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST
+    wp config create --allow-root \
+                    --path=/var/www/html/wordpress \
+                    --dbname=$WORDPRESS_DB_NAME \
+                    --dbuser=$WORDPRESS_DB_USER \
+                    --dbpass=$WORDPRESS_DB_USER_PASSWORD \
+                    --dbhost=$MYSQL_HOST
     echo "installing wordpress..."
-    wp core install --allow-root --path=/var/www/html/wordpress --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL
-
+    wp core install --allow-root \
+                    --path=/var/www/html/wordpress \
+                    --url=$DOMAIN_NAME \
+                    --title=$WORDPRESS_TITLE \
+                    --admin_user=$WORDPRESS_ADMIN_USER \
+                    --admin_password=$WORDPRESS_ADMIN_PASSWORD \
+                    --admin_email=$WORDPRESS_ADMIN_EMAIL
+    echo "creating general user..."
+    wp user create  --allow-root \
+                      $WORDPRESS_GENERAL_USER \
+                      $WORDPRESS_EMAIL \
+                    --role=$WORDPRESS_ROLE \
+                    --user_pass=$WORDPRESS_USER_PASSWORD
 
 
     chown -R www-data:www-data /var/www/html/wordpress
