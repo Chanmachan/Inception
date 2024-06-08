0d9aad6be00997a94a171ac86ec5a2ed134c5883
chanma <yunosuke.homma@icloud.com>
2024-06-08 21:15:37 +0900

    エラー

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -3,13 +3,11 @@
 set -e
 
 # mysqlのデータベースがない場合、初期設定
-if [ -d /var/lib/mysql/mysql ]; then
-    echo "mariadb is already installed"
-else
+if [ ! -d /var/lib/mysql/mysql ]; then
     echo "setting mariadb for the first time..."
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-    cat <<EOF > /tmp/input.txt
+        cat <<EOF > /tmp/input.txt
 USE mysql;
 FLUSH PRIVILEGES;
 CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
@@ -21,13 +19,17 @@ CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DB_NAME};
 CREATE USER IF NOT EXISTS '${WORDPRESS_DB_USER}'@'%' IDENTIFIED BY '${WORDPRESS_DB_USER_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO '${WORDPRESS_DB_USER}'@'%';
 FLUSH PRIVILEGES;
+
 EOF
-      # bootstrapで初期設定
+
+    # bootstrapで初期設定
     mysqld --user=mysql --bootstrap < /tmp/input.txt
 
     rm /tmp/input.txt
 
     echo "done setting mariadb"
+else
+    echo "mariadb is already installed"
 fi
 
 exec "$@"
