d0aadbd6d698062093c94e6193e5fab0837770f1
chanma <yunosuke.homma@icloud.com>
2024-06-08 19:01:08 +0900

    サーバを起動せずにビルド時にdbの設定をするように変更

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,50 +1,33 @@
 #!/bin/bash
 
-######################function######################
-timeout=30
-wait_server_start() {
-  while ! mysqladmin ping -u root --silent; do
-  sleep 1
-  timeout=$((timeout-1))
-  if [ "$timeout" -le 0 ]; then
-    echo "timeout"
-    exit 1
-  fi
-  echo "server started successfully"
-  done
-}
-####################################################
-
 set -e
 
 # mysqlのデータベースがない場合、初期設定
-if [ ! -d /var/lib/mysql/mysql ]; then
+if [ -d /var/lib/mysql/mysql ]; then
+    echo "mariadb is already installed"
+else
     echo "setting mariadb for the first time..."
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
-    echo "done setting mariadb"
-else
-    echo "mariadb is already installed"
-fi
 
-# サーバーを起動
-mysqld &
-wait_server_start
-# サーバーが起動している->設定   していない->exit
-# rootユーザーのパスワードを設定
-# データベース、ユーザー、権限を設定をまとめてしまった
-if ! mysql -u root -hlocalhost -p"${MYSQL_ROOT_PASSWORD}" -e "USE ${WORDPRESS_DB_NAME}" --silent; then
-  # TODO: "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';"はワイルドカードではなくmariadbとかでもいいかも
-  mysql -u root -hlocalhost -p"${MYSQL_ROOT_PASSWORD}" << EOF
-  CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-  GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
-  CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DB_NAME};
-  CREATE USER IF NOT EXISTS '${WORDPRESS_DB_USER}'@'%' IDENTIFIED BY '${WORDPRESS_DB_USER_PASSWORD}';
-  GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO '${WORDPRESS_DB_USER}'@'%';
-  FLUSH PRIVILEGES;
+    cat <<EOF > /tmp/input.txt
+USE mysql;
+FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
+
+CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DB_NAME};
+CREATE USER IF NOT EXISTS '${WORDPRESS_DB_USER}'@'%' IDENTIFIED BY '${WORDPRESS_DB_USER_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO '${WORDPRESS_DB_USER}'@'%';
+FLUSH PRIVILEGES;
 EOF
+      # bootstrapで初期設定
+    mysqld --user=mysql --bootstrap < /tmp/input.txt
+
+    rm /tmp/input.txt
+
+    echo "done setting mariadb"
 fi
-# サーバーを停止
-mysqladmin shutdown
-echo "done settings"
 
 exec "$@"
