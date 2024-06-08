23736b8e634ee486930c585847ca74b25f448da7
chanma <yunosuke.homma@icloud.com>
2024-06-08 19:34:05 +0900

    うまくいかないのでif文の条件を前に戻した

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -3,13 +3,11 @@
 set -e
 
 # mysqlのデータベースがない場合、初期設定
-if [ -d /var/lib/mysql/mysql ]; then
-    echo "mariadb is already installed"
-else
-    echo "setting mariadb for the first time..."
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+if [ ! -d /var/lib/mysql/mysql ]; then
+  echo "setting mariadb for the first time..."
+  mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-    cat <<EOF > /tmp/input.txt
+  cat <<EOF > /tmp/input.txt
 USE mysql;
 FLUSH PRIVILEGES;
 CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
@@ -22,12 +20,15 @@ CREATE USER IF NOT EXISTS '${WORDPRESS_DB_USER}'@'%' IDENTIFIED BY '${WORDPRESS_
 GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO '${WORDPRESS_DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-      # bootstrapで初期設定
-    mysqld --user=mysql --bootstrap < /tmp/input.txt
+  # bootstrapで初期設定
+  mysqld --user=mysql --bootstrap < /tmp/input.txt
 
-    rm /tmp/input.txt
+  rm /tmp/input.txt
 
-    echo "done setting mariadb"
+  echo "done setting mariadb"
+  echo "mariadb is already installed"
+else
+  echo "mariadb is already installed"
 fi
 
 exec "$@"
